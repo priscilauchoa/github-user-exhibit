1083b0f7cee033c3dea256e6d4e2b0eedcfd44db
Priscila Flores <priscilauchoaa@gmail.com>
2024-04-13 21:33:30 +0200

    create user profile container and handle functions

---
+++ src/containers/UserProfile.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Input from "../components/Input/Input";
+import Button from "../components/Button/Button";
+import fetchUsers from "../utils/fetchUsers";
+import { User } from "../interfaces/interfaces.ts"
+import UserDetails from "../components/UserDetails/UserDetails.tsx";
+
+export default function UserProfile() {
+  const [user, setUser] = useState<User[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [inputValue, setInputValue] = useState<string>("");
+  const [isUserDetailsVisible, setisUserDetailsVisible] = useState<boolean>(false);
+
+  async function fetchData(inputValue: string) {
+    try {
+      const data = await fetchUsers(inputValue);
+      setUser(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+  // set the input value to a variable state
+  const handleOnChange = (value) => {
+    setInputValue(value)
+  }
+
+  // call fetch data on onclick passing the input value as parameter 
+  const handleOnClick = () => {
+    fetchData(inputValue)
+    setisUserDetailsVisible(true);
+  }
+
+
+  return (
+    <>
+      <div>
+        <Input label="User" onChange={handleOnChange} />
+        <Button onClick={handleOnClick} title="Search" />
+        {isUserDetailsVisible && <UserDetails src={user.avatar_url} name={user.name} login={user.login} error={error} />}
+      </div>
+    </>
+  );
+}
