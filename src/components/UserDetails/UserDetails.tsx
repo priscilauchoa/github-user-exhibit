00543d10d2df924bb25c08ab1a1197390b2a085c
Priscila Flores <priscilauchoaa@gmail.com>
2024-04-14 12:21:42 +0200

    add interface to props

--- src/components/UserDetails/UserDetails.tsx
+++ src/components/UserDetails/UserDetails.tsx
@@ -1,9 +1,15 @@
-import { UserDetailsProps } from '../../interfaces/interfaces.ts'
-function UserDetails({ avatar_url, login, name, error }: UserDetailsProps) {
+interface UserDetailsProps {
+    src: string;
+    login: string;
+    name: string;
+    error: boolean;
+}
+
+function UserDetails({ src, login, name, error }: UserDetailsProps) {
     return (
         <>
             {login &&
-                <div><img className="avatar" src={avatar_url} alt="" />
+                <div><img className="avatar" src={src} alt="" />
                     <h4>Login: {login}</h4>
                     <p>Login: {name}</p></div>}
             {!login && <h2 className='red'>{error} The user does not exist or the data was probably not available</h2>}
