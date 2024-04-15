f59f491e3c4e50df9217cf069dda3674de0ccf11
Priscila Flores <priscilauchoaa@gmail.com>
2024-04-15 11:53:48 +0200

    change type of function paramenter and return to generic

--- src/utils/fetchGithubData.ts
+++ src/utils/fetchGithubData.ts
@@ -1,12 +1,7 @@
-interface User {
-    id: number;
-    valueInput?: string;
-  }
-  
-  async function fetchGithubData(valueInput): Promise<User[]> {
-    const response = await fetch(`https://api.github.com/users/${valueInput}`);
-    const data = await response.json();
-    return data;
-  }
-  
-  export default fetchGithubData;
+async function fetchGithubData<T>(userLogin: string): Promise<T> {
+  const response = await fetch(`https://api.github.com/users/${userLogin}`);
+  const data = await response.json();
+  return data;
+}
+
+export default fetchGithubData;
