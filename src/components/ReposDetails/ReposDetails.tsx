6efa4adea2f108a69f23946d8dcd921faced1c7c
Priscila Flores <priscilauchoaa@gmail.com>
2024-06-10 17:39:57 +0200

    install styled-components and add css for ReposDetails

--- src/components/ReposDetails/ReposDetails.tsx
+++ src/components/ReposDetails/ReposDetails.tsx
@@ -1,18 +1,25 @@
+import styled from 'styled-components'
+import RepoInfo from './ReposDetails'
+
 interface RepoDetailsProps {
     name: string;
     visibility: string;
     description: string;
 }
 
+// const RepoInfo = styled.h2`
+//     margin: 10px 0;
+//     color: violet;
+// `
+
 function ReposDetails({ name, description, visibility }: RepoDetailsProps) {
     return (
         <>
-            <h1 className="red">Repo: {name} </h1>
-            <h2 className='red'>
-                {visibility}
-                {description}
-            </h2>
-            <br />
+            <RepoInfo red={true}>
+                <h2 className="red">Repo: {name} </h2>
+                <p>{visibility} </p>
+                <p>{description} </p>
+            </RepoInfo>
         </>
     )
 }
